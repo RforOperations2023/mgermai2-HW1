8659039417d64729b05775ee7e730e19074b681b
mgermaine93 <mgermaine93@gmail.com>
2023-02-01 14:50:44 -0500

    Saving progress on server part for now.

--- app.R
+++ app.R
@@ -11,13 +11,13 @@ TRANSIT_DATA <- read.csv(
   "data/prt-transit.csv",
 )
 
+TRANSIT_DATA <- data.frame(TRANSIT_DATA)
+
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
   # theme = shinytheme("darkly"),
-  
-  # ui stuff goes in here
-  
+  title = "PRT",
   # displays the title of the app
   titlePanel(
     "PRT Ridership, 2017-2022",
@@ -40,21 +40,6 @@ ui <- fluidPage(
         startview = "year"
       ),
       
-      # enables the user to select which days of data to view
-      checkboxGroupInput(
-        inputId = "day_of_the_week",
-        label = "Select which days:",
-        choices = c(
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday"
-        )
-      ),
-      
       # enables the user to view the most popular routes
       sliderInput(
         inputId = "top",
@@ -63,6 +48,61 @@ ui <- fluidPage(
         max = 10,
         value = 1
       ),
+      
+      # enables the user to select which routes to view
+      selectInput(
+        inputId = "route_name",
+        label = "Route Name",
+        choices = unique(TRANSIT_DATA$route_full_name),
+        multiple = TRUE,
+        selectize = TRUE
+      ),
+      
+      # filter by day
+      # enables the user to select which days of data to view
+      checkboxGroupInput(
+        inputId = "day_type",
+        label = "Select a Day Type:",
+        choiceNames = list(
+          "Weekday",
+          "Saturday",
+          "Sunday/Holiday"
+        ),
+        choiceValues = list(
+          "WEEKDAY",
+          "SAT.",
+          "SUN."
+        ),
+      ),
+      checkboxInput(
+        inputId = "allDayTypes",
+        label = "Select all day types"
+      ),
+      
+      
+  
+      # enables the user to view ridership details
+      radioButtons(
+        inputId = "statType", 
+        label = "Statistic:",
+        c(
+          "Average Riders" = "avg_riders",
+          "Current Garage" = "current_garage"
+        ),
+        inline = TRUE
+      ),
+      
+      # dataframe stuff
+      DT::dataTableOutput(outputId = "topRoutesDT"),
+      
+      # # enables the user to download the data
+      downloadButton(
+        outputId = "prt_ridership",
+        label = "Download Data Table"
+      )
+    
+      
+      
       
       # # enables the user to select how many modes to display
       # checkboxGroupInput(
@@ -74,37 +114,6 @@ ui <- fluidPage(
       #     "Incline"
       #   )
       # ),
-      
-      # enables the user to select which routes to view
-      selectInput(
-        inputId = "route_name",
-        label = "Route Name",
-        choices = unique(TRANSIT_DATA$route_full_name),
-        multiple = TRUE,
-        selectize = TRUE
-      ),
-      
-      # enables the user to view ridership details
-      radioButtons(
-        inputId = "statType", 
-        label = "Statistic:",
-        c(
-          "Average Riders" = "avg_riders",
-          "Current Garage" = "Average.Points"
-          ),
-        inline = TRUE
-      ),
-      
-      # dataframe stuff
-      DT::dataTableOutput(
-        outputId = "topRoutesDT",
-      ),
-      
-      # # enables the user to download the data
-      downloadButton(
-        outputId = "prt_ridership",
-        label = "Download Data Table"
-      )
       
     ),
     
@@ -125,23 +134,42 @@ ui <- fluidPage(
 # server logic goes in here
 server <- function(input, output) {
   
-  # Filter on user input for date, top n players, and country
+  # Filter on user input for date, day type, and top routes.
   user_filtered <- reactive({
-      # Referenced this to blog for how to use dplyr to filter dataframe based on date range:
-      # https://www.r-bloggers.com/2022/06/what-is-the-best-way-to-filter-by-date-in-r/
-      TRANSIT_DATA %>% filter(between(month_start, input$dates[1], input$dates[2])) %>% filter(avg_riders <= input$top)
+    if (input$allDayTypes) {
+      TRANSIT_DATA %>% filter(between(month_start, input$dates[1], input$dates[2])) %>%
+        filter(TRANSIT_DATA <= input$top)
+    } else {
+      TRANSIT_DATA %>% filter(between(month_start, input$dates[1], input$dates[2])) %>% 
+        filter(avg_riders <= input$top) %>% filter(day_type %in% input$day_type)
+    }
+  })
+  user_filtered
+  
+  # filter on individual route stats
+  route_stats <- reactive({
+    TRANSIT_DATA %>% filter(route_full_name == input$route)
   })
   
-  
-  # Render plot for Average Points for top x players
   output$mostPopularRoutes <- renderPlot({
-    ggplot(data = user_filtered(), aes(x = month_start, y = avg_riders, color=route)) +
-      geom_line() + geom_point() +
-      ggtitle(paste("Average Ridership for Top", input$top, "PRT Routes", sep = " ")) +
-      # Referenced for how to center plot title
-      # https://stackoverflow.com/questions/40675778/center-plot-title-in-ggplot2
-      theme(plot.title = element_text(hjust = 0.5))
+    
   })
+  
+  
+  
+  
+  
+  
+  
+  # # Render plot for average ridership for top x routes
+  # output$mostPopularRoutes <- renderPlot({
+  #   ggplot(data = user_filtered(), aes(x = route, y = avg_riders, color=route)) +
+  #     geom_line() + geom_point() +
+  #     ggtitle(paste("Average Ridership for Top", input$top, "PRT Routes", sep = " ")) +
+  #     # Referenced for how to center plot title
+  #     # https://stackoverflow.com/questions/40675778/center-plot-title-in-ggplot2
+  #     theme(plot.title = element_text(hjust = 0.5))
+  # })
   
 }
 
